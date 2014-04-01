95a764e7c1f4fa6cbcd1c76d824ec8eff3095097
Robin <wb10.80@gmail.com>
2014-04-01 15:11:02 +0200

    entity_game returns for each property added

--- src/alice/entity_game.hpp
+++ src/alice/entity_game.hpp
@@ -11,9 +11,7 @@ namespace dota {
     class entity_game {
         public:
             /** Constructor */
-            entity_game(entity* eGame) : eGame(eGame) {
-
-            }
+            entity_game(entity* eGame) : eGame(eGame) { }
 
             /** Default copy constructor */
             entity_game(const entity_game&) = default;
@@ -26,107 +24,138 @@ namespace dota {
 
             /** Get the ID of this match */
             uint32_t getMatchId() {
-                // .dota_gamerules_data.m_unMatchID
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_unMatchID");
             }
 
             /** Returns name of this lobby */
             std::string getLobbyName() {
-                // .dota_gamerules_data.m_lobbyGameName
+                return eGame->prop<std::string>(".dota_gamerules_data.m_lobbyGameName");
             }
 
             /** Returns the number of players that joined the lobby */
             uint32_t getExpectedPlayers() {
-                // .dota_gamerules_data.m_nExpectedPlayers
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_nExpectedPlayers");
             }
 
             /** Returns the number of players that correctly joined the game */
             uint32_t getConnectedPlayers() {
-                // .dota_gamerules_data.m_nLoadedPlayers
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_nLoadedPlayers");
             }
 
             /** Returns Dire wins if this game is in a series */
             uint32_t getDireWins() {
-                // .dota_gamerules_data.m_nDireSeriesWins
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_nDireSeriesWins");
             }
 
             /** Returns Radiant wins if this game is in a series */
             uint32_t getRadiantWins() {
-                // .dota_gamerules_data.m_nRadiantSeriesWins
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_nRadiantSeriesWins");
             }
 
             /** Returns league ID if applicable */
             uint32_t getLeagueID() {
-                // .dota_gamerules_data.m_lobbyLeagueID
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_lobbyLeagueID");
             }
 
             /** Returns the game mode */
             uint32_t getGameMode() {
-                 // .dota_gamerules_data.m_iGameMode
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_iGameMode");
             }
 
             /** Returns whether this game is running the stable mode */
             bool isStableMode() {
-                // .dota_gamerules_data.m_bIsStableMode
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_bIsStableMode") == 1;
             }
 
             /** Returns the time at which the game has been started */
             uint32_t getGameDate() {
-                // .dota_gamerules_data.m_iNetTimeOfDay
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_iNetTimeOfDay");
             }
 
             /** Returns the current state of the game */
             uint32_t getState() {
-                // .dota_gamerules_data.m_nGameState
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_nGameState");
             }
 
             /** Returns the current hero pick state based on the game mode */
             uint32_t getPickState() {
-                 // .dota_gamerules_data.m_nHeroPickState
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_nHeroPickState");
             }
 
             /** Returns the team that started picking */
             uint32_t getPickTeamStart() {
-                // .dota_gamerules_data.m_iStartingTeam
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_iStartingTeam");
             }
 
             /** Returns the current team picking */
             uint32_t getPickTeamActive() {
-                // .dota_gamerules_data.m_iActiveTeam
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_iActiveTeam");
             }
 
             /** Returns player numbers of captains */
             std::vector<int32_t> getCaptains() {
-                // .dota_gamerules_data.m_iCaptainPlayerIDs.0000 - 1
+                std::vector<int32_t> captains{
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_iCaptainPlayerIDs.0000"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_iCaptainPlayerIDs.0001")
+                };
+
+                return captains;
             }
 
             /** Returns the pick / ban order */
             uint32_t getPickBanOrder() {
-                // .dota_gamerules_data.m_iCMModePickBanOrder
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_iCMModePickBanOrder");
             }
 
             /** Returns extra pick time remaining for the radiant */
             float getRadiantPickTime() {
-                // .dota_gamerules_data.m_fExtraTimeRemaining.0000
+                return eGame->prop<float>(".dota_gamerules_data.m_fExtraTimeRemaining.0000");
             }
 
             /** Returns extra pick time remaining for the dire */
             float getDirePicktime() {
-                // .dota_gamerules_data.m_fExtraTimeRemaining.0001
+                return eGame->prop<float>(".dota_gamerules_data.m_fExtraTimeRemaining.0001");
             }
 
             /** Returns banned heroes in order, always 10, - 1 if not applicable */
             std::vector<int32_t> getBanned() {
-                // .dota_gamerules_data.m_BannedHeroes.0000 - 09
+                std::vector<int32_t> banned{
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_BannedHeroes.0000"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_BannedHeroes.0001"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_BannedHeroes.0002"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_BannedHeroes.0003"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_BannedHeroes.0004"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_BannedHeroes.0005"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_BannedHeroes.0006"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_BannedHeroes.0007"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_BannedHeroes.0008"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_BannedHeroes.0009")
+                };
+
+                return banned;
             }
 
-            /** Returns number of returned heroes */
+            /** Returns array of selected heroes */
             std::vector<int32_t> getSelected() {
-                // .dota_gamerules_data.m_SelectedHeroes.0000 - 09
+                std::vector<int32_t> selected{
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_SelectedHeroes.0000"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_SelectedHeroes.0001"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_SelectedHeroes.0002"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_SelectedHeroes.0003"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_SelectedHeroes.0004"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_SelectedHeroes.0005"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_SelectedHeroes.0006"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_SelectedHeroes.0007"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_SelectedHeroes.0008"),
+                    eGame->prop<int32_t>(".dota_gamerules_data.m_SelectedHeroes.0009")
+                };
+
+                return selected;
             }
 
             /** Returns current ingame time */
             float getTime() {
-                // .dota_gamerules_data.m_fGameTime
+                return eGame->prop<float>(".dota_gamerules_data.m_fGameTime");
             }
 
             /** Returns overall time spend including picking / pre-game phase */
@@ -136,62 +165,63 @@ namespace dota {
 
             /** The time the game loaded */
             float getTimeLoaded() {
+                return eGame->prop<float>(".dota_gamerules_data.m_fGameTime");
                  // .dota_gamerules_data.m_flGameLoadTime
             }
 
             /** Get the time the pregame started */
             float getTimePregame() {
-                // .dota_gamerules_data.m_flPreGameStartTime
+                return eGame->prop<float>(".dota_gamerules_data.m_flPreGameStartTime");
             }
 
             /** Get the time the game started */
             float getTimeGamestart() {
-                // .dota_gamerules_data.m_flGameStartTime
+                return eGame->prop<float>(".dota_gamerules_data.m_flGameStartTime");
             }
 
             /** Time the game ended if applicable */
             float getTimeEnd() {
-                // .dota_gamerules_data.m_flGameEndTime
+                return eGame->prop<float>(".dota_gamerules_data.m_flGameEndTime");
             }
 
             /** Whether the game is currently paused */
             bool isPaused() {
-                // .dota_gamerules_data.m_bGamePaused
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_bGamePaused") == 1;
             }
 
             /** The team who paused the game */
             uint32_t getPauseTeam() {
-                // .dota_gamerules_data.m_iPauseTeam
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_iPauseTeam");
             }
 
             /** Cooldown of radiant glyph if applicable */
             float getGlyphCooldownRadiant() {
-                // .dota_gamerules_data.m_fGoodGlyphCooldown
+                return eGame->prop<float>(".dota_gamerules_data.m_fGoodGlyphCooldown");
             }
 
             /** Cooldown of dire glpyh if applicable */
             float getGlyphCooldownDire() {
-                // .dota_gamerules_data.m_fBadGlyphCooldown
+                return eGame->prop<float>(".dota_gamerules_data.m_fBadGlyphCooldown");
             }
 
             /** Returns the team that called GG */
             uint32_t getGGTeam() {
-                // .dota_gamerules_data.m_nGGTeam
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_nGGTeam");
             }
 
             /** Time the game ends because one team called GG */
             float getGGTime() {
-                // .dota_gamerules_data.m_flGGEndsAtTime
+                return eGame->prop<float>(".dota_gamerules_data.m_flGGEndsAtTime");
             }
 
             /** Whether this night is caused due to nightstalker using his ultimate */
             bool isNightstalkerNight() {
-                // .dota_gamerules_data.m_bIsNightstalkerNight
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_bIsNightstalkerNight") == 1;
             }
 
             /** Returns the winner of the game if applicable */
             uint32_t getWinner() {
-                // .dota_gamerules_data.m_nGameWinner
+                return eGame->prop<uint32_t>(".dota_gamerules_data.m_nGameWinner");
             }
 
             // CDOTAGamerulesProxy
